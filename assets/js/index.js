02d4cae0bb07ce3794410009387314add61c3b43
rongyueHu <1752333046@qq.com>
2022-05-22 20:02:00 +0800

    已完成

--- assets/js/index.js
+++ assets/js/index.js
@@ -34,22 +34,22 @@ function getUserInfo() {
             renderAvatar(res.data);
         },
         // 不论成功还是失败，最终都会调用 complete 回调函数
-       /*  complete: (res) => {
-            // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
-            if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
-                //  强制清空 token
-                localStorage.removeItem("token");
-                // 强制跳转到登录页面
-                location.href = "/login.html"
-            }
-        }, */
+        /*  complete: (res) => {
+             // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+             if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+                 //  强制清空 token
+                 localStorage.removeItem("token");
+                 // 强制跳转到登录页面
+                 location.href = "/login.html"
+             }
+         }, */
     });
 };
 
 // 渲染用户头像
-let renderAvatar = (user) => {
+const renderAvatar = (user) => {
     // 获取用户名字
-    let name = user.nickname || user.username;
+    const name = user.nickname || user.username;
     // 设置欢迎文本
     $("#welcome").html(`欢迎 ${name}`);
     // 按需渲染用户头像
@@ -64,3 +64,7 @@ let renderAvatar = (user) => {
         $(".text-avatar").html(firstName);
     }
 };
+
+function change() {
+    $('#art_list').addClass('layui-this').next().removeClass('layui-this')
+}
